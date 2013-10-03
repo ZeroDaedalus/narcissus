b84a1db4580df0fbcf30e7f3af34764d14116b8f
DaJoker29 <dj729@comcast.net>
2013-10-03 15:26:05 -0700

    added more templates

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title><?php bloginfo('name'); ?></title>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <link rel="stylesheet" type="text/css" src="<?php bloginfo('stylesheet_url'); ?>" />
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <h1>HEADER</h1>
+        </header>
+
