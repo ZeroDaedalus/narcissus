acb036e785469efced3d995869a6ac0d3bf3c3ee
DaJoker29 <dj729@comcast.net>
2013-10-04 12:59:14 -0700

    switched to _s template, added sass, built header

--- header.php
+++ header.php
@@ -1,12 +1,40 @@
-<html>
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Narcissus
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
-    <title><?php bloginfo('name'); ?></title>
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/stylesheets/style.css" />
-</head>
-<body>
-    <div id="wrapper">
-        <header>
-            <h1>HEADER</h1>
-        </header>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<?php wp_head(); ?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/stylesheets/style.css" />
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<?php do_action( 'before' ); ?>
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h4 class="site-description"><?php bloginfo( 'description' ); ?></h4>
+		</div>
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<h1 class="menu-toggle"><?php _e( 'Menu', 'narcissus' ); ?></h1>
+			<div class="screen-reader-text skip-link"><a href="#content"><?php _e( 'Skip to content', 'narcissus' ); ?></a></div>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+    <hr />
+	<div id="content" class="site-content">
--- index.php
+++ index.php
@@ -1,16 +1,46 @@
-<?php get_header(); ?>
-<section id="main">
-    <section id="content">
-        <h1>Main Area</h1>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <h4>Posted on <?php the_time('F jS, Y') ?></h4>
-        <p><?php the_content(__('(more...)')); ?></p>
-        <hr /> <?php endwhile; else: ?>
-        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-    </section>
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Narcissus
+ */
 
-    <?php get_sidebar(); ?>
-</section>
+get_header(); ?>
 
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php narcissus_content_nav( 'nav-below' ); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'no-results', 'index' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
