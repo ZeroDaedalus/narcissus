085b1406d796c20dd4fbb53789a76b27aa54cd90
DaJoker29 <dj729@comcast.net>
2013-10-18 18:31:12 -0700

    add front-page template and remove header - issue #31

--- content-page.php
+++ content-page.php
@@ -7,11 +7,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
+    <?php if( !in_array( 'home', get_body_class() ) ) { ?>
+        <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </header><!-- .entry-header -->
+    <?php }	?>
+    
+        <div class="entry-content">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
